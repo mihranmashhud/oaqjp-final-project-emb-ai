4ab3dabc63f06bc1cae156e852c5bc82c4cad497
mihranmashhud <mihranmashhud@gmail.com>
2025-05-13 14:46:45 -0400

    Reformat output.

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze: str):
-    resp = requests.get(
+    resp = requests.post(
         "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict",
         headers={
             "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
@@ -12,4 +12,16 @@ def emotion_detector(text_to_analyze: str):
 
     json_resp = json.loads(resp.text)
 
-    return json_resp
+    emotions = json_resp["emotionPredictions"][0]["emotion"]
+
+    dominant_emotion_score = 0
+    dominant_emotion = ""
+    for emotion, score in emotions.items():
+        if score > dominant_emotion_score:
+            dominant_emotion_score = score
+            dominant_emotion = emotion
+
+    emotions["dominant_emotion"] = dominant_emotion
+    return emotions
+
+
